45b8e67dcafb00b4588bb366a55a4f68065bdc2a
casey wilson <Casey>
2021-08-25 23:17:55 -0400

    initial

---
+++ atlasLiveChat/atlasLiveChat.API/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using atlasLiveChat.API.Hubs.Clients;
+using atlasLiveChat.API.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace atlasLiveChat.API.Hubs
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+        public async Task SendMessage(ChatMessage message)
+        {
+            await Clients.All.ReceiveMessage(message);
+        }
+    }
+}
---
+++ atlasLiveChat/atlasLiveChat.API/Hubs/Clients/IChatClient.cs
@@ -0,0 +1,13 @@
+﻿using atlasLiveChat.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace atlasLiveChat.API.Hubs.Clients
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(ChatMessage message);
+    }
+}
